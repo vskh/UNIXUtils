d07a8237bf5a6dd7a40610b4b468137c21e9e133
Vadym S. Khondar <vadym@khondar.name>
2016-06-19 15:22:03 +0300

    Add framework for UNIX cat arguments handling

--- cat/Program.cs
+++ cat/Program.cs
@@ -4,12 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace cat
+namespace Khondar.UNIXUtils.Concat
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
+            var options = new Options();
+            if (CommandLine.Parser.Default.ParseArguments(args, options))
+            {
+                if (options.Version)
+                {
+                    var assemblyInfo = typeof(Program).Assembly.GetName();
+                    Console.WriteLine(assemblyInfo.Name + " v" + assemblyInfo.Version);
+                }
+
+                if (options.ShowAll)
+                {
+                    options.ShowNonPrintingWithEnds = true;
+                    options.ShowNonPrintingWithTabs = true;
+                }
+
+                if (options.ShowNonPrintingWithEnds)
+                {
+                    options.ShowNonPrinting = true;
+                    options.ShowEnds = true;
+                }
+
+                if (options.ShowNonPrintingWithTabs)
+                {
+                    options.ShowNonPrinting = true;
+                    options.ShowTabs = true;
+                }
+            }
         }
     }
 }
