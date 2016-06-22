ab6348ee1b779848e4a785487388eacbced319f3
Vadym S. Khondar <vadym@khondar.name>
2016-06-22 23:01:21 +0300

    Add core of mkdir.

---
+++ mkdir/Options.cs
@@ -0,0 +1,23 @@
+﻿using CommandLine;
+using CommandLine.Text;
+
+namespace Khondar.UNIXUtils.MakeDirectory
+{
+    internal class Options
+    {
+        [Option('m', "mode", HelpText = "set file mode (as in chmod), not a=rwx - umask")]
+        public string Mode { get; set; }
+        [Option('p', "parents", HelpText = "no error if existing, make parent directories as needed")]
+        public bool Parents { get; set; }
+        [Option('v', "verbose", HelpText = "print message for each created directory")]
+        public bool Verbose { get; set; }
+        [Option('V', "version", HelpText = "output version information and exit")]
+        public bool Version { get; set; }
+
+        public string GetUsage()
+        {
+            return HelpText.AutoBuild(this,
+              (HelpText current) => HelpText.DefaultParsingErrorsHandler(this, current));
+        }
+    }
+}
---
+++ mkdir/Program.cs
@@ -0,0 +1,16 @@
+﻿using CommandLine;
+
+namespace Khondar.UNIXUtils.MakeDirectory
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            var options = new Options();
+            if (Parser.Default.ParseArguments(args, options))
+            {
+                
+            }
+        }
+    }
+}
