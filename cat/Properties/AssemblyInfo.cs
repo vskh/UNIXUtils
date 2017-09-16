652eca6f68d8099a1863e4b4491964c40bc78a9c
Vadym S. Khondar <vadym@khondar.name>
2017-09-16 15:11:04 +0100

    Improve usage info for cat.

--- cat/Properties/AssemblyInfo.cs
+++ cat/Properties/AssemblyInfo.cs
@@ -1,17 +1,25 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using CommandLine;
 
 // Управление общими сведениями о сборке осуществляется с помощью 
 // набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
 // связанные со сборкой.
 [assembly: AssemblyTitle("cat")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Concatenate files and print result to console")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("khondar.name")]
 [assembly: AssemblyProduct("cat")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+
+[assembly: AssemblyLicense("This is free software." +
+	" You may redistribute copies of it under the terms of the MIT License" +
+   	" <http://www.opensource.org/licenses/mit-license.php>.\n")]
+[assembly: AssemblyUsage("Usage: cat [options]... [files]...\n" +
+                         "Concatenate given files (or take content of stardard input if none given)" +
+                         " and print it to console.")]
 
 // Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
 // для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
